01e020b9253515559c1f2e3a58bdbe4c004035c3
Niole Nelson <niolenelson@gmail.com>
2015-11-08 21:06:19 -0500

    line car moves naturally, steers by approximating new alignment between current and most desirable

--- js/vehicle.js
+++ js/vehicle.js
@@ -16,6 +16,7 @@ class Vehicle {
     this.steer = 0;
     this.thrust = 0;
     this.maxUpdateVel = maxVel/updateFreq; //max number of px covered per update
+    this.maxVel = maxVel;
     this.targetX = this.x1;
     this.targetY = this.y1;
   }
@@ -23,6 +24,35 @@ class Vehicle {
   updateTargetState( x, y) {
     this.targetX = x;
     this.targetY = y;
+  }
+
+  getCarSlope(pos) {
+    let slope = (pos.y1 - pos.y2)/(pos.x1 - pos.x2);
+    if ( isNaN(slope)) {
+      return 0;
+    }
+    return slope;
+  }
+
+  getAlignment(pos) {
+    let alignment = (pos.y1 - pos.targetY)/(pos.x1 - pos.targetX);
+    if ( isNaN(alignment)) {
+      return 0;
+    }
+    return alignment;
+  }
+
+  updatedAlignment(carSlope, alignment) {
+    return (carSlope + alignment)/2;
+  }
+
+  setTailPos(newAlignment, pos) {
+    // newAlignment = O/A, want hyp to be CAR_LENGTH
+    let radians = Math.atan(newAlignment);
+    let adjacent = Math.cos(radians)*CAR_LENGTH;
+    let opposite = Math.sin(radians)*CAR_LENGTH;
+    this.x2 = this.x1 - adjacent;
+    this.y2 = this.y1 - opposite;
   }
 
   updateVehicleState(height, width) {
@@ -34,17 +64,19 @@ class Vehicle {
     let dy = yConst*this.maxUpdateVel;
     this.x1 += dx;
     this.y1 += dy
-    this.setTailVal(this.x1, this.y1, dx, dy);
+    this.setTailVal(this);
   }
 
-  setTailVal(x1, y1, dx, dy) {
+  setTailVal(vehicle) {
    //gets hypotenuse of mini triangle, gets
    //number of times it goes into car length,
    //then subtracts the respective sides by that constanct
-   let miniHyp = Math.sqrt((Math.pow(dx, 2) + Math.pow(dy, 2)));
-   let K = CAR_LENGTH/miniHyp;
-   this.x2 -= dx;
-   this.y2 -= dy;
+//   this.x2 += dx;
+ //  this.y2 += dy;
+    let carSlope = this.getCarSlope(this);
+    let alignment = this.getAlignment(this);
+    let newAlignment = this.updatedAlignment(carSlope, alignment);
+    this.setTailPos(newAlignment, this);
   }
 }
 
